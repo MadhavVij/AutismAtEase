9292ee7137f52bc16b22aeab3f5f48a95b4f9b3a
anshul <anshul.sogani28@gmail.com>
2018-05-01 05:59:41 -0500

    guess me

--- app/src/main/java/com/project3/www/AutismAtEase/click.java
+++ app/src/main/java/com/project3/www/AutismAtEase/click.java
@@ -1,13 +1,52 @@
 package com.project3.www.AutismAtEase;
 
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class click extends AppCompatActivity {
-
+    private ImageButton clicker;
+    private Boolean status = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_click);
+
+        clicker = (ImageButton) findViewById(R.id.clicker);
+        final Drawable drawable = clicker.getDrawable();
+        final Vibrator vibe = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
+
+        clicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                vibe.vibrate(100);
+
+
+
+                Log.d(AppSettings.tagAS, "onClick: "+drawable.getState());
+                Log.d(AppSettings.tagAS, "HUHUHU "+getResources().getDrawable(R.drawable.unclicked).getState());
+
+
+                if(status == true)
+                {
+                    clicker.setImageResource(R.drawable.clicked);
+                    status = false;
+                }else {
+                    clicker.setImageResource(R.drawable.unclicked);
+                    status = true;
+                }
+
+
+
+            }
+        });
     }
 }
--- app/src/main/java/com/project3/www/AutismAtEase/guess.java
+++ app/src/main/java/com/project3/www/AutismAtEase/guess.java
@@ -2,12 +2,37 @@ package com.project3.www.AutismAtEase;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class guess extends AppCompatActivity {
+
+    private TextView sad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guess);
+
+    }
+
+    public void happy(View view) {
+        sad = (TextView) findViewById(R.id.textView4);
+        sad.setText("HAPPY");
+
+    }
+
+    public void sad(View view) {
+        sad = (TextView) findViewById(R.id.textView4);
+        sad.setText("SAD");
+    }
+    public void angry(View view) {
+        sad = (TextView) findViewById(R.id.textView4);
+        sad.setText("ANGRY");
+    }
+    public void excited(View view) {
+        sad = (TextView) findViewById(R.id.textView4);
+        sad.setText("EXCITED");
     }
 }
